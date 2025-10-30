f6311cd196d578ccc752c9ac75562526e298cd27
Andrew <Zarelly2013@yandex.com>
2025-10-31 01:11:19 +0500

    test4

--- Lesson1/main.cpp
+++ Lesson1/main.cpp
@@ -5,6 +5,6 @@
 int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
-    std::cout << "Test3" << std::endl;
+    std::cout << "Test4" << std::endl;
     return 0;
 }
